f52aa95bf8bcfdeaabf835c2a297c5be08ea15e9
v <v@mail.ru>
2024-08-21 15:18:23 +0500

    models: мастера и услуги

--- core/models.py
+++ core/models.py
@@ -26,3 +26,33 @@ class Visit(models.Model):
     class Meta:
         verbose_name = "Запись"
         verbose_name_plural = "Записи"
+
+
+# Класс для мастеров
+class Master(models.Model):
+    first_name = models.CharField(max_length=100, verbose_name='Имя')
+    last_name = models.CharField(max_length=100, verbose_name='Фамилия')
+    phone = models.CharField(max_length=20, verbose_name='Телефон')
+    address = models.CharField(max_length=255, verbose_name='Домашний адрес')
+    photo = models.ImageField(upload_to='masters/photos/', blank=True, null=True, verbose_name='Фотография')
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name} - {self.phone}'
+    
+    class Meta:
+        verbose_name = "Мастер"
+        verbose_name_plural = "Мастера"
+
+# Класс для услуг
+class Service(models.Model):
+    name = models.CharField(max_length=200, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена')
+    masters = models.ManyToManyField(Master, related_name='services', verbose_name='Мастера')
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = "Услуга"
+        verbose_name_plural = "Услуги"
