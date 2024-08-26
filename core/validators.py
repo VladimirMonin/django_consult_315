d07e33a842905bb623de4fb8449eebc43fabc1da
v <v@mail.ru>
2024-08-26 18:46:14 +0500

    forms: валидация простой формы и вывод ошибки

---
+++ core/validators.py
@@ -0,0 +1,8 @@
+def super_validator(value):
+    """
+    Тестовый валидатор, который всегда возвращает True.
+    """
+    if not value:
+        False
+    else:
+        True
--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Visit
+from .validators import super_validator
 
 
 def main(request):
@@ -9,17 +10,28 @@ def main(request):
         phone = request.POST.get('phone')
         comment = request.POST.get('comment', '')
 
+        # Валидация данных
+        if super_validator(name) and super_validator(phone):
+            # Данные валидны, продолжаем обработку
+            
         
-        # Создаем новую запись посещения в базе данных
-        Visit.objects.create(
-            name=name,
-            phone=phone,
-            comment=comment,
+            # Создаем новую запись посещения в базе данных
+            Visit.objects.create(
+                name=name,
+                phone=phone,
+                comment=comment,
 
-        )
-        
-        # Перенаправляем на страницу благодарности после успешного создания записи
-        return redirect('thanks')
+            )
+            
+            # Перенаправляем на страницу благодарности после успешного создания записи
+            return redirect('thanks')
+    
+        else:
+            # Данные не валидны, выводим сообщение об ошибке
+            error_message = "Пожалуйста, заполните все поля корректно."
+            # Возвращаем ошибку в шаблон (на уровне шаблона нужна првоерка if)
+            return render(request, "main.html", {"error_message": error_message})
+    
     
     # Обработка GET-запроса или других типов запросов
     return render(request, 'main.html')
