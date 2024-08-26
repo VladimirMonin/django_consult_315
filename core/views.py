6a15bd329e6b759288048ef7f126d4190773ce36
v <v@mail.ru>
2024-08-26 18:11:43 +0500

    forms: подготовка для разбора

--- core/views.py
+++ core/views.py
@@ -4,21 +4,22 @@ from .models import Visit
 
 
 def main(request):
-    if request.method == 'POST':
-        form = VisitForm(request.POST)
-        if form.is_valid():
-            # Создание и сохранение записи в модели Visit
-            Visit.objects.create(
-                name=form.cleaned_data['name'],
-                phone=form.cleaned_data['phone'],
-                comment=form.cleaned_data.get('comment', '')
-            )
-            # Перенаправляем пользователя на страницу с благодарностью после успешного сохранения
-            return redirect('thanks')
-    else:
-        form = VisitForm()
+    pass
+    # if request.method == 'POST':
+    #     form = VisitForm(request.POST)
+    #     if form.is_valid():
+    #         # Создание и сохранение записи в модели Visit
+    #         Visit.objects.create(
+    #             name=form.cleaned_data['name'],
+    #             phone=form.cleaned_data['phone'],
+    #             comment=form.cleaned_data.get('comment', '')
+    #         )
+    #         # Перенаправляем пользователя на страницу с благодарностью после успешного сохранения
+    #         return redirect('thanks')
+    # else:
+    #     form = VisitForm()
 
-    return render(request, 'main.html', {'form': form})
+    # return render(request, 'main.html', {'form': form})
 
 
 def thanks(request):
