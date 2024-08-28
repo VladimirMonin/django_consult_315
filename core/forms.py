35c432131a05fac19cca8f96b43b57e6840d2091
v <v@mail.ru>
2024-08-28 16:49:07 +0500

    lesson_63: переход на форму не связанную с моделью

--- core/forms.py
+++ core/forms.py
@@ -1,1 +1,18 @@
 from django import forms
+
+
+class VisitForm(forms.Form):
+    # Классы form-control, placeholder
+    name = forms.CharField(label='Имя', max_length=100, widget=forms.TextInput(attrs={'placeholder': 'Имя', 'class': 'form-control'}))
+    phone = forms.CharField(label='Телефон', max_length=20, widget=forms.TextInput(attrs={'type': 'tel', 'placeholder': 'Номер телефона', 'class': 'form-control'}))
+    comment = forms.CharField(label='Комментарий', required=False, widget=forms.Textarea(attrs={'placeholder': 'Комментарий', 'class': 'form-control'}))
+
+    # Валидация данных данных происходит через расширение\переопределене методов
+    # clean_<field_name> например, проверим что каждый элемент имени является строкой
+    def clean_name(self):
+        name = self.cleaned_data["name"]
+        
+        if not isinstance(name, str):
+            # Поднимаем исключение, которое попадет в контекст шаблона
+            raise forms.ValidationError("Имя должно быть строкой")
+        return name
--- core/views.py
+++ core/views.py
@@ -1,40 +1,34 @@
 from django.shortcuts import render, redirect
 from .models import Visit
 from .validators import super_validator
+from .forms import VisitForm
 
 
 def main(request):
     if request.method == 'POST':
-        # Получаем данные из формы
-        name = request.POST.get('name')
-        phone = request.POST.get('phone')
-        comment = request.POST.get('comment', '')
+        # Создаем экземпляр формы
+        form = VisitForm(request.POST)
 
-        # Валидация данных
-        if super_validator(name) and super_validator(phone):
-            # Данные валидны, продолжаем обработку
-            
-        
+        # Проверяем, что форма валидна
+        if form.is_valid():
             # Создаем новую запись посещения в базе данных
-            Visit.objects.create(
-                name=name,
-                phone=phone,
-                comment=comment,
-
+            visit = Visit(
+                # cleaned_data - словарь с очищенными данными из формы, после валидации
+                name=form.cleaned_data["name"],
+                phone=form.cleaned_data["phone"],
+                comment=form.cleaned_data["comment"],
             )
-            
+            visit.save()
             # Перенаправляем на страницу благодарности после успешного создания записи
             return redirect('thanks')
     
         else:
-            # Данные не валидны, выводим сообщение об ошибке
-            error_message = "Пожалуйста, заполните все поля корректно."
-            # Возвращаем ошибку в шаблон (на уровне шаблона нужна првоерка if)
-            return render(request, "main.html", {"error_message": error_message})
+            # Если форма не валидна, передаем ее в контекст шаблона
+            return render(request, "main.html", {"form": form})
     
-    
-    # Обработка GET-запроса или других типов запросов
-    return render(request, 'main.html')
+    else:
+        # Обработка GET-запроса или других типов запросов
+        return render(request, "main.html", {"form": VisitForm()})
 
 
 def thanks(request):
