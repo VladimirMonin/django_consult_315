0a1227e8f2ad19e0c50bde975b9219421c28595c
v <v@mail.ru>
2024-08-28 19:09:54 +0500

    lesson_64: JS - динамически подгружаем услуги для мастеров

--- barbershop/urls.py
+++ barbershop/urls.py
@@ -1,13 +1,18 @@
 from django.contrib import admin
 from django.urls import path
-from core.views import main, thanks
+from core.views import main, thanks, get_services_by_master
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', main),
-    path('thanks/', thanks, name='thanks'),
+    path("admin/", admin.site.urls),
+    path("", main),
+    path("thanks/", thanks, name="thanks"),
+    path(
+        "get_services_by_master/<int:master_id>/",
+        get_services_by_master,
+        name="get_services_by_master",
+    ),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- core/views.py
+++ core/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import VisitModelForm
-from .models import Visit, Master
-
+from .models import Visit, Master, Service
+from django.http import JsonResponse
 
 def main(request):
     if request.method == 'POST':
@@ -12,9 +12,16 @@ def main(request):
     else:
         form = VisitModelForm()
 
+    # Мастера для карусели фоточек (в форму данные берутся и по мастерам и по услугам автоматически)
     masters = Master.objects.all()
     return render(request, 'main.html', {'form': form, 'masters': masters})
 
 
 def thanks(request):
     return render(request, 'thanks.html')
+
+def get_services_by_master(request, master_id):
+    services = Master.objects.get(id=master_id).services.all()
+    services_data = [{'id': service.id, 'name': service.name} for service in services]
+    return JsonResponse({'services': services_data})
+
