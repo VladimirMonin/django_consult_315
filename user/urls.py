4bd674b4bb5da6dfa06e768460463602f55e47ad
v <v@mail.ru>
2024-09-09 18:35:25 +0500

    lesson_67: все максимально упростили и использовали служебные инструменты

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
 from .views import login_view, logout_view, register_view
-
+from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
 
 urlpatterns = [
-    path('login/', login_view, name='login'),
-    path('logout/', logout_view, name='logout'),
-    path('register/', register_view, name='register'),
-]   
+    path('login/', LoginView.as_view(template_name='login.html',
+                                     next_page='main'), name='login'),
+    path('logout/', LogoutView.as_view(next_page='main', http_method_names=['get', 'post'],
+                                       template_name="logout.html"), name='logout'),
+    path('register/', CreateView.as_view(
+        template_name='register.html',
+        form_class=UserCreationForm,
+        success_url='/'
+    ), name='register'),
+]
