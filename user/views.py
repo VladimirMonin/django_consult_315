367a1e923a9736e917638a78b2e46081246b166c
v <v@mail.ru>
2024-09-09 18:12:58 +0500

    lesson_67: регистрация

--- user/views.py
+++ user/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.models import User
 
 def login_view(request):
     if request.method == 'POST':
@@ -12,8 +13,32 @@ def login_view(request):
             return redirect('main')
         else:
             messages.error(request, 'Неверные учетные данные')
+    
+    # Метод GET
     return render(request, 'login.html')
 
+def register_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+        
+        if password == confirm_password:
+            if not User.objects.filter(username=username).exists():
+                user = User.objects.create_user(username=username, password=password)
+                # Логиним и редиректим 
+                login(request, user)
+                return redirect('main')
+            else:
+                error = 'Пользователь с таким именем уже существует'
+        else:
+            error = 'Пароли не совпадают'
+        
+        # Негативный случай. Мы показываем ошибку пользователю
+        return render(request, 'register.html', {'error': error})
+    
+    # Метод GET
+    return render(request, 'register.html')
 def logout_view(request):
     logout(request)
     return redirect('main')
