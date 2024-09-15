985ae5e5cf202744580ac7464ce07783b798b996
v <v@mail.ru>
2024-09-15 11:29:54 +0500

    fix: поправил logout

--- user/urls.py
+++ user/urls.py
@@ -7,8 +7,7 @@ from django.contrib.auth.forms import UserCreationForm
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='login.html',
                                      next_page='main'), name='login'),
-    path('logout/', LogoutView.as_view(next_page='main', http_method_names=['get', 'post'],
-                                       template_name="logout.html"), name='logout'),
+    path('logout/', logout_view, name='logout'),
     path('register/', CreateView.as_view(
         template_name='register.html',
         form_class=UserCreationForm,
