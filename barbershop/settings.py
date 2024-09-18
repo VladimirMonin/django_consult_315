e80508c2d8a13f3a7c2b3e1e67b5bd450bb2820e
v <v@mail.ru>
2024-09-18 19:07:20 +0500

    lesson_68: восстновление пароля через email

--- barbershop/settings.py
+++ barbershop/settings.py
@@ -151,3 +151,17 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',  # Стандартный бекенд для аутентификации по username
     "user.authentication.EmailAuthBackend",  # Наш бекенд для аутентификации по email
 ]
+
+# Настройка SMTP сервера
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' # Вывод сообщений в консоль
+
+
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_USE_SSL = True
+DEFAULT_FROM_EMAIL = os.getenv('EMAIL_HOST_USER')
+SERVER_EMAIL = os.getenv('EMAIL_HOST_USER')
+EMAIL_ADMIN = os.getenv('EMAIL_HOST_USER')
