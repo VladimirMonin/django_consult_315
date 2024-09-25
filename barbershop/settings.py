7762d16d14990cbb622cd5320583dfb4824c5715
v <v@mail.ru>
2024-09-25 17:48:03 +0500

    lesson_70: подготовка к деплою

--- barbershop/settings.py
+++ barbershop/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*", "194.87.84.253"]
 
 
 # Application definition
